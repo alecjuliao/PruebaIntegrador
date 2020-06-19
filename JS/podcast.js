fa0972cdb21c4dc8a63d556380eb4055f50da7b3
Marcos Hogg <marcoshogg@MacBook-Air-de-Jimena.local>
2020-06-18 23:57:31 -0300

    podcast

--- JS/podcast.js
+++ JS/podcast.js
@@ -1,21 +1,54 @@
-window.onload = function(){
+window.onload =function(){
 
-fetch ("https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/0/podcasts")
-.then(function(respuesta){
-    return respuesta.json();
-   })
-   .then(function(resultadoFinal){
-    console.log(resultadoFinal.data);
-    var generos = document.querySelector("ul#podcastMas");
-    for (var i = 0; i < resultadoFinal.data.length; i++){
-       console.log(resultadoFinal.data[i].name);
-       generos.innerHTML += '<li>'+'<a href="podcastdetalles.html?id='+ resultadoFinal.data[i].id +'&name='+ resultadoFinal.data[i].title +'">'+ '<img src="'+resultadoFinal.data[i].picture+'">' + resultadoFinal.data[i].title +'</a>' + '</li>'
-       console.log(generos);
-     }
-   })
 
-   /*.catch(function(error) {
-    console.log("Error: " + error);
-  })*/
+
+
+
+
+  fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/0/podcasts')
+  .then(function(response) {
+    return response.json()
+  })
+  .then(function(information) {
+    console.log(information);
+   
+    var contenedorPodcasts = document.querySelector('.lista-podcasts')
+    var contenido =''
+     
+      for (let i = 0; i < information.data.length; i++) {
+        const element = information.data[i];
+        console.log(element)
+      
+        contenido += '<li class="informacion-podcast">'
+        contenido += '<div class="imagen-podcast">'
+        contenido += '<img src="'+element.picture_medium+'" alt="">'
+        contenido += '<p>Fans: '+element.fans+'</p>'
+        contenido += '</div>'
+        contenido += '<div class="informacion">'
+        contenido += '<h4><a href="#">'+element.title+'</a></h4>'
+        contenido += '<p>Descripcion: <br>'+element.description+'</p>'
+        contenido += '</div>'
+        contenido += '</li>'
+
+        
+
+
+      }
+
+        
+      contenedorPodcasts.innerHTML = contenido
+      
+ 
+ 
+ 
+   
+ 
+   })
+   .catch(function(error) {
+      console.log("Error: " + error);
+    })
+  
+
+
 
 }
