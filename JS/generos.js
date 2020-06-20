244fd4457702c29a90f5aa365e2b400fb8f2c995
Marcos Hogg <marcoshogg@MacBook-Air-de-Jimena.local>
2020-06-20 14:36:51 -0300

    generos mejora diseño y loading gif

--- JS/generos.js
+++ JS/generos.js
@@ -1,5 +1,5 @@
 window.onload = function(){
-    
+    document.querySelector('.img-generos1').innerHTML = '<img class="loading" src="../img/loading gif.gif" alt="loading"</img>'
     fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre')
 
     .then(function(response){
@@ -7,11 +7,14 @@ window.onload = function(){
        })
        .then(function(resultadoFinal){
         console.log(resultadoFinal.data);
-        var generos = document.querySelector("ul.img-generos1");
- 
+        var generos = document.querySelector(".img-generos1");
+ var contenido = ''
         for (var i = 1; i < resultadoFinal.data.length; i++){
            console.log(resultadoFinal.data[i].name);
-           generos.innerHTML += '<li>'+'<a href="Ingeneros.html?id='+ resultadoFinal.data[i].id +'&name='+ resultadoFinal.data[i].name +'">'+ '<img src="https://api.deezer.com/genre/' + resultadoFinal.data[i].id + '/image">' +'<h2>'+ resultadoFinal.data[i].name+'</h2>' +'</a>' + '</li>'
+           
+           contenido += '<li><a href="Ingeneros.html?id='+ resultadoFinal.data[i].id +'&name='+ resultadoFinal.data[i].name +'"><img src="https://api.deezer.com/genre/' + resultadoFinal.data[i].id + '/image"><h2>'+ resultadoFinal.data[i].name+'</h2></a></li>'
+           
+           generos.innerHTML = contenido
            console.log(generos);
          }
        })
